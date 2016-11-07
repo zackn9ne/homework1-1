a3e647d9ef4ebae833c7a9c5b3dd26a4a77e984b
kjdavis2423@gmail.com <kingdavis2423@gmail.com>
2016-11-07 13:02:53 -0500

    Yo

--- js/app.js
+++ js/app.js
@@ -1,19 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
+console.log("Yo");
 
+var Twit = require('twit');
 
-// Create Div element
-var div = document.createElement('DIV');
-//Set height of element
-div.style.height = '100vh';
-// Append Element to DOM
-document.body.appendChild(div);
-// Add Event Listener to Element
-div.addEventListener('mousemove', function(event){
-    console.log(event);
-    var x = event.clientX;
-    var y = event.clientY;
-    div.textContent = x + ',' + y;
-    div.style.backgroundColor = 'rgb(' + x + ', ' + y + ', 100)';
-});
-});
+var config = require('./config');
+console.log(config);
 
+// var T = new Twit({
+
+// });
---
+++ js/config.js
@@ -0,0 +1,10 @@
+
+
+module.exports = {
+  consumer_key:         'KQWPeJ9MzAi8pJXI3rm5FP4d6',
+  consumer_secret:      'ahouRN0djcGdOyAtgG16lZT3YCLYpSAFYuMGcPT0LgrGrDXQKb',
+  access_token:         '277255496-LHem4MsjxAh5B5ED0aYGBJMUjK0mQaWllWsHTl2d',
+  access_token_secret:  'w0vMTKr81k29S1ryYkIannEKX3XMsyJCLfCACL8ZfN9pl',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+	
+}
