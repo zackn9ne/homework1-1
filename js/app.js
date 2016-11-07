2ebab8bfb6313cf52e039ab2c79e5d2e0b2d1eca
kjdavis2423@gmail.com <kingdavis2423@gmail.com>
2016-11-07 16:29:15 -0500

    What up

--- js/app.js
+++ js/app.js
@@ -3,8 +3,33 @@ console.log("Yo");
 var Twit = require('twit');
 
 var config = require('./config');
-console.log(config);
 
-// var T = new Twit({
+var T = new Twit(config);
 
-// });
+
+
+
+
+
+
+
+
+var params = { 
+	q: 'for', 
+	count: 3
+};
+
+T.get('search/tweets', params, gotData);
+
+
+function gotData(err, data, response) {
+  // console.log(data.statuses[0].created_at)
+  var tweets = data.statuses;
+  	console.log(tweets.length);
+    for (var i = 0;i < tweets.length; i++){
+  	console.log(tweets[i].text);
+
+  }
+
+};
+
