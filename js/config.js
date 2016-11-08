abf1b3f6462ca52defbca0b38e5787705f0db50b
kjdavis2423@gmail.com <kingdavis2423@gmail.com>
2016-11-08 12:10:00 -0500

    What up

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 
 
 module.exports = {
-  consumer_key:         'KQWPeJ9MzAi8pJXI3rm5FP4d6',
-  consumer_secret:      'ahouRN0djcGdOyAtgG16lZT3YCLYpSAFYuMGcPT0LgrGrDXQKb',
-  access_token:         '277255496-LHem4MsjxAh5B5ED0aYGBJMUjK0mQaWllWsHTl2d',
-  access_token_secret:  'w0vMTKr81k29S1ryYkIannEKX3XMsyJCLfCACL8ZfN9pl',
+  consumer_key:         'W65c3IJKsNMF35OEI8Kxff7Dn',
+  consumer_secret:      'uchs5Gp1W3aLWHdAr1Xm4Zai4PLOIlXPBKegpKtdIjF6cryKUp',
+  access_token:         '2970659225-b0Yrsu20APBLsmuPluQkuFAIkGU3llk1Th9HMJN',
+  access_token_secret:  'yl4RC1St6ZeYIyxfyeBOOrSj6NCBXqP3kziDR7wV4BiTn',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-	
+
 }
